12d036c9a85e71b5afcbb0edb9f9b0bbb89b7c51
luap-trams <luap.trams@gmail.com>
2024-09-18 23:06:12 +0200

    bug: mobile responsiveness

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -8,14 +8,14 @@ import "@fontsource/roboto/latin-500.css";
 export default function Home() {
   return (
     // <div className='bg-amber-600 h-screen text-white flex flex-row justify-center items-center text-6xl'>
-  <div className='text-center'>
+  <div className='md:text-center'>
       
 
-          <p className='text-8xl py-20 inline-flex justify-center text-center text-blue-900 font-bold'>Unlock the door to your dream home</p>
+          <h1 className='text-4xl md:text-8xl py-20 inline-flex justify-center text-center text-blue-900 font-bold'>Unlock the door to your dream home</h1>
   
 
         <div className='inline-flex flex-wrap justify-center gap-4 w-full'>
-          <Image src={'/aboutcover.jpeg'} alt={'Tall building'} width={'1400'} height={'1500'}/>
+          <Image src={'/aboutcover.jpeg'} alt={'home cover'} width={'1400'} height={'1500'}/>
           
        
          
@@ -24,8 +24,8 @@ export default function Home() {
         </div>
 
           <div className='py-3'>
-          <h2 className='flex justify-center text-center text-4xl text-black-900 px-5'>Exclusive Luxury Properties in Lagos</h2>
-          <p className='flex justify-center text-center text-md'>Curated selection of the market's finest listings, chosen just for you.</p>
+          <h2 className='flex justify-center text-center text-2xl md:text-4xl px-4'>Exclusive Luxury Properties in Lagos</h2>
+          <p className='flex justify-center text-center text-sm md:text-md'>Curated selection of the market's finest listings, chosen just for you.</p>
 
           <div className='flex items-center justify-center my-2'><span className='bg-blue-900 text-white rounded-t-2xl p-2.5 uppercase'>Top Picks</span>
           </div>
--- src/app/prop/[slug]/page.jsx
+++ src/app/prop/[slug]/page.jsx
@@ -25,7 +25,7 @@ export default function PropDetails({params}) {
   </Link>);
 
 const otherImages = details.images?.map((image, index) => (
-  <img src={image} alt={`${details.title} image ${index + 1}`} className="w-1/3 mx-auto" key={index} />
+  <img src={image} alt={`${details.title} image ${index + 1}`} className='rounded-md mx-auto p-1' key={index} />
 ));
 
 const featureList = details.features?.map((feature, index) => (
@@ -35,15 +35,17 @@ const featureList = details.features?.map((feature, index) => (
 
   return (
     <div className='text-center'>
-        <h1>{details.title}</h1>
-        <div className='flex justify-center mx-auto px-10 gap-x-8'>
+        <h1 className='md:text-2xl'>{details.title}</h1>
+        {/* <div className='flex justify-center mx-auto px-10 gap-x-8'> */}
+        <div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
           {otherImages}
         </div>
         <p className='flex place-items-center flex-nowrap mx-auto p-5 bg-transparent h-1/3 w-1/3'>{details.description}</p>
         {/* <img src={details.images[0]} alt={details.title} className="w-1/3 mx-auto" /> */}
         <div className='features-list'>
-            <h2 className='text-2xl font-bold'>Features</h2>
+            <h2 className='text-lg font-bold'>Features</h2>
               <ul className='md:inline-flex gap-5 mx-auto'>
+
                 {featureList}
               </ul>
       </div>
@@ -52,7 +54,7 @@ const featureList = details.features?.map((feature, index) => (
         <button onClick={handlePropAdd} className='bg-blue-900 text-white p-3 rounded-md m-2'>add property</button>
 
             <div>
-              <h2 className='text-4xl font-bold'>related properties</h2>
+              <h2 className='text-4xl font-bold p-2'>Related properties</h2>
 
               <div className='grid grid-cols-1 md:grid-cols-3 gap-4'>
                 {relatedCard}
