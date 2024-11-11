ad46cf81bee9c85dab03c3d044f029d6fef595a0
luap-trams <luap.trams@gmail.com>
2024-11-11 21:03:40 +0100

    Style: reposition search bar to the top right

--- src/app/prop/page.jsx
+++ src/app/prop/page.jsx
@@ -22,7 +22,14 @@ export default function PropertyPage() {
       <section key={index} className='rounded-lg shadow-2xl'>
       
       <Link href={`/prop/${card.title.split(' ').join('-')}`} title={card.title} className='cursor-pointer'>
-        <PropertyCard title={card.title} location={card.location} category={card.category} type={card.type} price={card.price} rating={card.rating} status={card.status}/>
+        <PropertyCard 
+        title={card.title} 
+        location={card.location} 
+        category={card.category} 
+        type={card.type} 
+        price={card.price} 
+        rating={card.rating} 
+        status={card.status}/>
       </Link>
     </section>
     ))
@@ -31,13 +38,15 @@ export default function PropertyPage() {
     <>
     {/* <div className='flex items-center justify-between p-3 border-4 rounded-full mt-4 border-blue-400 bg-white w-full sm:w-3/4 md:w-1/2 lg:w-2/5 xl:w-1/3 m-auto'></div> */}
     
-    <div className='flex items-center justify-between p-3 border-2 rounded-full mt-4 border-none bg-white w-3/4 max-sm:w-3/4 md:w-1/2 lg:w-2/5 xl:w-1/3 m-auto'>
+    <div className='inset-0 md:mr-11 fixed flex items-center border-2 mt-1.5 border-none rounded-sm bg-white w-1/2 md:w-1/5 m-auto h-7'>
     
-    <input type="text" name='search' id='search' className='outline-none block w-4/5 text-xl' placeholder='Search available properties…' defaultValue={searchProp} onChange={(e)=>filterSearch(e)} style={{ backgroundColor: 'transparent'}}/>
+    {/* <input type="text" name='search' id='search' className='outline-none block w-4/5 text-xl' placeholder='Search available properties…' defaultValue={searchProp} onChange={(e)=>filterSearch(e)} style={{ backgroundColor: 'transparent'}}/> */}
+    <input type="text" name='search' id='search' className='p-4  outline-none block w-4/5 text-sm' placeholder='Search available properties…' defaultValue={searchProp} onChange={(e)=>filterSearch(e)} style={{ backgroundColor: 'transparent'}}/>
     
-    <IoMdSearch className='text-4xl mr-2 my-8-[50px] w-[50px] p-[6px] rounded-full bg-blue-400'/>
-    </div>
+    <IoMdSearch className=' text-2xl -mr-28 my-8-[50px] w-[50px] p-[6px] rounded-full'/>
 
+    </div>
+    
     <div className='grid grid-cols-1 gap-4 px-8 py-16 bg-blue-200 h-full max-sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
     {cards}
     </div>
